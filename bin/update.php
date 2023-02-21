10f0343207bc8045a3501a1a277eecab778e5edb
Ricardo Boss <contact@ricardoboss.de>
2023-02-21 15:40:14 +0100

    Add logging to update.php

--- bin/update.php
+++ bin/update.php
@@ -6,3 +6,6 @@ $updateUrl = "https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.
 $destinationFile = dirname(__DIR__) . "/data/mime.types";
 
 file_put_contents($destinationFile, file_get_contents($updateUrl));
+
+echo "Downloaded mime.types from '$updateUrl' and stored at '$destinationFile'" . PHP_EOL;
+echo PHP_EOL;
